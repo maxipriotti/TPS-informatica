ec51dfc0e21ac9ac0c49fda9c0330ba54b2d567a
maxipriotti <164506178+maxipriotti@users.noreply.github.com>
2025-06-22 23:14:16 -0300

    Update bmi.c

--- bmi.c
+++ bmi.c
@@ -12,29 +12,42 @@ int main(int argc, char *argv[]) {
 	float altura;
 	float imc;
 	
-	printf ("ingrese su altura en metros ");
-	scanf ("%f", &altura);
+	// Validar que la altura sea positiva
+	do {
+		printf("ingrese su altura en metros: ");
+		scanf("%f", &altura);
+		if (altura <= 0) {
+			printf("ERROR: la altura debe ser un número positivo\n");
+		}
+	} while (altura <= 0);
 	
-	printf ("ingrese su peso en kg ");
-	scanf ("%f", &peso);  
+	// Validar que el peso sea positivo
+	do {
+		printf("ingrese su peso en kg: ");
+		scanf("%f", &peso);
+		if (peso <= 0) {
+			printf("ERROR: el peso debe ser un número positivo\n");
+		}
+	} while (peso <= 0);  
 	
 	imc = (peso / (altura * altura));
-		
-if (imc < 18.5){
-		printf ("Bajo peso");
+	
+	if (imc < 18.5) {
+		printf("Bajo peso");
 	}
 	
-	if (imc > 18.5 && imc < 24.9 ){
-		printf ("Normal");
+	if (imc > 18.5 && imc < 24.9) {
+		printf("Normal");
 	}
 	
-	if (imc > 25 && imc < 29.9){
-		printf ("Sobrepeso");
+	if (imc > 25 && imc < 29.9) {
+		printf("Sobrepeso");
 	}
 	
-	if (imc > 30){
-		printf ("Obesidad");
+	if (imc > 30) {
+		printf("Obesidad");
 	}
-return 0;
+	
+	return 0;
 }
 
